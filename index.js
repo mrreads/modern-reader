1553926b5cffb137a43072f7b8628470c29be860
Alexander <mrreads@yandex.com>
2020-08-08 03:08:19 +0300

    electron init

---
+++ index.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () 
+{
+    let win = new BrowserWindow(
+    {
+        width: 800,
+        height: 600,
+        minWidth: 400,
+        minHeight: 400,
+        frame: false,
+        webPreferences: {
+        nodeIntegration: true
+        }
+    });
+
+    win.loadFile('./app/index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => 
+{
+    if (process.platform !== 'darwin') 
+    {
+        app.quit()
+    }
+});
+
+app.on('activate', () => 
+{
+    if (BrowserWindow.getAllWindows().length === 0) 
+    {
+        createWindow();
+    }
+});
