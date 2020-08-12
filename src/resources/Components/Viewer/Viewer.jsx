609305561b71671310c9c121b004934ea2eb13ac
Alexander <mrreads@yandex.com>
2020-08-12 17:28:35 +0300

    dynamic change setting view

--- src/resources/Components/Viewer/Viewer.jsx
+++ src/resources/Components/Viewer/Viewer.jsx
@@ -2,6 +2,7 @@ import React, { useState} from 'react';
 import { withRouter  } from 'react-router-dom';
 
 const fs = window.require('fs');
+const userPath = require('./../../../data').userPath;
 
 function Viewer(props)
 {
@@ -21,7 +22,29 @@ function Viewer(props)
     {
         textBook = fs.readFileSync(book.path, 'utf8');
     }
-    const [content, updateContent] = useState(textBook)
+    const [content, updateContent] = useState(textBook);
+
+    const settings = JSON.parse(fs.readFileSync(userPath.settings, 'utf8'));
+    const [padding, updatePadding] = useState(settings.padding);
+    const [fontSize, updateFontSize] = useState(settings.fontSize);
+    const [lineHeight, updateLineHeight] = useState(settings.lineHeight);
+
+    const checkValue = (e) =>
+    {
+        if (e.target.dataset.type === 'padding')
+        {
+            updatePadding(e.target.value);
+        }
+        if (e.target.dataset.type === 'fontSize')
+        {
+            updateFontSize(e.target.value);
+        }
+        if (e.target.dataset.type === 'lineHeight')
+        {
+            updateLineHeight(e.target.value);
+        }
+    }
+
 
      return (
         <div id="content" className="view">
@@ -39,28 +62,33 @@ function Viewer(props)
                     
                     <div className="inputNumberWrapper font-size">
                         <p> Font-Size: <span></span></p>
-                        <input type="range" min="11" max="46" />
+                        <input type="range" min="11" max="46" onInput={checkValue} data-type="fontSize" />
                     </div>
 
                     <div className="hr"></div>
 
                     <div className="inputNumberWrapper padding">
                         <p> Padding: <span></span></p>
-                        <input type="range" min="0" max="50" />
+                        <input type="range" min="0" max="50" onInput={checkValue} data-type="padding" />
                     </div>
 
                     <div className="hr"></div>
 
                     <div className="inputNumberWrapper line-height">
                         <p> Line-height: <span></span></p>
-                        <input type="range" min="0.5" max="2" step="0.1" />
+                        <input type="range" min="0.5" max="2" step="0.1" onInput={checkValue} data-type="lineHeight" />
                     </div>
 
                 </div>
 
                 <h1> { props.location.data.path } </h1>
 
-                <div className="content"> { content }</div>
+                <div className="content" 
+                    style={{
+                        padding: padding + 'px',
+                        fontSize: fontSize + 'px',
+                        lineHeight: lineHeight
+                        }}> { content } </div>
 
             </div>
 
