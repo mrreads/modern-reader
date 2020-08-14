ebb6c45b6ebdfe17016dd135276035a961388d36
Alexander <mrreads@yandex.com>
2020-08-14 16:38:19 +0300

    fb2 stylized

--- src/resources/Components/Books/Book/formats/RenderFb2.jsx
+++ src/resources/Components/Books/Book/formats/RenderFb2.jsx
@@ -14,17 +14,27 @@ function RenderFb2(props)
     const fb2book = new FB2HTML(fb2data);
     const bookData = 
     {
+        title: fb2book.getTitle(),
         image: fb2book.getCover(),
         author: fb2book.getAuthors(),
     }
     return (
     <Link to={{ pathname: "/viewer", data: { book: props.data } }} className={`book ${extClass}`}>
-        <p className={(fs.existsSync( props.data.path)) ? "name" : "name error" }> <strong> {props.data.name} </strong> </p>
+        <img src={bookData.image} />
+        
+        <div className="wrapper">
+            <div className="mainInfo">
+                <p className={(fs.existsSync( props.data.path)) ? "name" : "name error" }> <strong> {props.data.name} </strong> </p>
+                <p className="title"> {bookData.title} </p>
+                <p className="author"> {bookData.author} </p>
+           </div>
 
-        <div className="info">
-            <p className="format">Format: <i> { props.data.ext} </i> </p>
-            <p>Strings: <i> { props.data.strings} </i> </p>
+            <div className="info">
+                <p className="format">Format: <i> { props.data.ext} </i> </p>
+                <p>Strings: <i> { props.data.strings} </i> </p>
+            </div>
         </div>
+
     </Link>);
 }
 
