0f3ede733e2f411fda849c44ef4cb2f8290d4d4f
Alexander <mrreads@yandex.com>
2020-08-16 18:59:47 +0300

    class -> func

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,44 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Book from './Book/Book';
-import Modal from './Modal/Modal';;
+import Modal from './Modal/Modal';
 
-class Books extends React.Component
+export default function(props)
 {
-    constructor(props)
+    const [getModalStatus, setModalStatus] = useState(false);
+
+    const showModal = () =>
     {
-        super(props);
-        this.state = { show: false }
+        setModalStatus(!getModalStatus);
     }
 
-    showModal = () =>
-    {
-        this.setState({show: !this.state.show});
-    }
-
-    render(props)
-    {
-        return (
-        <div id="content" className="books">
-            <div className="title"> 
-                <p> Library </p>
-                <div href="#" className="add" id="addPopup" onClick={this.showModal}>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus" width="44" height="44" viewBox="0 0 24 24" fill="none">
-                        <path stroke="none" d="M0 0h24v24H0z"/>
-                        <line x1="12" y1="5" x2="12" y2="19" />
-                        <line x1="5" y1="12" x2="19" y2="12" />
-                    </svg>
-                </div>
+    return (
+    <div id="content" className="books">
+        <div className="title">
+            <p> Library </p>
+            <div className="add" id="addPopup" onClick={ showModal }>
+                <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-plus" width="44" height="44" viewBox="0 0 24 24" fill="none">
+                    <path stroke="none" d="M0 0h24v24H0z"/>
+                    <line x1="12" y1="5" x2="12" y2="19" />
+                    <line x1="5" y1="12" x2="19" y2="12" />
+                </svg>
             </div>
+        </div>
 
-            <hr/>
-            
-            <div className="books">
-                { this.props.books.getBooks.map((book, i) => <Book data={book} key={i} /> ) }
-            </div>
+        <hr/>
 
-            <Modal show={this.state.show} onClose={() => this.showModal()} toUpdate={ this.props.books.setBooks } />
-        </div>);
-    }
+        <div className="books">
+            { props.books.getBooks.map((book, i) => <Book data={book} key={i} /> ) }
+        </div>
+
+        <Modal show={ getModalStatus } onClose={ showModal } toUpdate={ props.books.setBooks } />
+    </div>);
 }
-
-export default Books;
