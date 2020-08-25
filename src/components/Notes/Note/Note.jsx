2f5e70c93ee6675ededb3d657e19254cee909f35
Alexander <mrreads@yandex.com>
2020-08-25 15:28:44 +0300

    delete note

--- src/components/Notes/Note/Note.jsx
+++ src/components/Notes/Note/Note.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import { IconButton, ButtonToolbar, Icon } from 'rsuite';
+
 export default function(props)
 {
-    return ( <div className="note"> <p> { props.data } </p> </div> );
+    return (
+    <div className="note"> 
+        <p> { props.data } </p>
+        <ButtonToolbar className="delete" onClick={ e => props.delete(e, props.data) }>
+            <IconButton icon={ <Icon icon="trash"/> } /> 
+        </ButtonToolbar>
+    </div> );
 }
--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -5,9 +5,21 @@ import { Divider } from 'rsuite';
 
 import { useTranslation } from 'react-i18next';
 
+const fs = window.require('fs');
+const userPath = require('./../../storage').userPath;
+
 export default function(props)
 {
     const { t } = useTranslation('notes');
+
+    const deleteNote = (e, text) =>
+    {
+        let allNotes = [...props.notes.getNotes];
+        let forDelete = text;
+        let newNotes = allNotes.filter(note => note !== forDelete);
+        fs.writeFileSync(userPath.notes, JSON.stringify(newNotes));
+        props.notes.setNotes(newNotes);
+    }
 
     return (
     <div id="content">
@@ -15,6 +27,6 @@ export default function(props)
             <h4> { t('notes') } </h4>
         </div>
         <Divider />
-        { props.notes.getNotes.map((note, i) => <Note data={ note } key={i} /> ) }
+        { props.notes.getNotes.map((note, i) => <Note delete={ deleteNote } data={ note } key={i} /> ) }
     </div>);
 }
