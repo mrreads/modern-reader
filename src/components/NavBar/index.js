efaac18294f68383509e12d0e559c274430bf0ea
Alexander <mrreads@yandex.com>
2022-07-06 01:56:12 +0300

    path on hover current book

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -7,25 +7,37 @@ import { ReactComponent as Setting } from '@/images/icons/setting.svg';
 import { observer } from 'mobx-react-lite';
 import useStore from '@/hooks/useStore'
 
+import Tooltip from '@/components/Tooltip';
+
 import './index.scss';
 
 const NavBar = observer(() => {
     const [ libraryStore ] = useStore('library');
-    const { current } = libraryStore;
+    const { current, getCurrentBook } = libraryStore;
 
     return (
         <div className='navbar-wrapper'>
 
             {(() => {
                 if (current == null)
-                    return (<div className='navbar-element disable'> <Book fill='#FFFFFF' height={28} width={28} /> </div>)
+                    return (<div className='navbar-element disable'> 
+                        <Book fill='#FFFFFF' height={28} width={28} /> 
+                        </div>)
                 else
-                    return (<NavLink to="/render" className='navbar-element'> <Book fill='#FFFFFF' height={28} width={28} /> </NavLink>)
+                    return (<Tooltip text={getCurrentBook().title} align="right" customStyles={{ width: '100%' }} noWordWrap>
+                                <NavLink to="/render" className='navbar-element'> 
+                                    <Book fill='#FFFFFF' height={28} width={28} /> 
+                                </NavLink>
+                            </Tooltip>)
             })()}
 
-            <NavLink to="/" className='navbar-element'> <Library fill='#FFFFFF' height={30} width={30} /> </NavLink>
+            <NavLink to="/" className='navbar-element'> 
+                <Library fill='#FFFFFF' height={30} width={30} />
+            </NavLink>
 
-            <NavLink to="/setting" className='navbar-element'> <Setting fill='#FFFFFF' height={32} width={32} /> </NavLink>
+            <NavLink to="/setting" className='navbar-element'>
+                <Setting fill='#FFFFFF' height={32} width={32} />
+            </NavLink>
         </div>
     );
 });
